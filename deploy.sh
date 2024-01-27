99fba5fe433597c5ab761dd54baaaf62056f9db5
domicmeia <dhdlqks55@gmail.com>
2024-01-27 22:25:54 +0900

    rebuilding site 2024년 1월 27일 토요일 22시 25분 54초 KST

---
+++ deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Update post from til repository.
+git submodule update --remote
+
+# Build the project.
+# hugo -t <your theme>
+hugo -t github-style-plus
+
+# Go to public folder, submodule commit
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin gh-pages
+
+# Come back up to the project root
+cd ..
+
+# Commit and push to main branch
+git add .
+
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin main
